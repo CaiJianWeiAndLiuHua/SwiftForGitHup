4fe8921c4a380ce38608774b65d1ba84288508b1
caijianwei <249133529@qq.com>
2016-01-18 08:54:20 +0800

    jwcai
    addNew work

---
+++ DemoSwiftJwcaiCoreWork/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  DemoSwiftJwcaiCoreWork
+//
+//  Created by CaixiaoLiu on 16/1/17.
+//  Copyright © 2016年 jw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("height :\(Device.height)  width: \(Device.width) deviceName: \(Device.deviceName)")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+
+target 'DemoSwiftJwcaiCoreWork' do
+
+
+    pod 'AFNetworking', '~> 2.6.0'
+
+
+    pod 'ReactiveCocoa','~> 2.5'
+
+
+    pod 'MJRefresh', '~> 2.4.11'
+    
+    pod 'ProgressHUD', '~> 1.3'
+    
+    
+    
+end
+
